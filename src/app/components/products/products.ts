63fbc67cdee0a0a9ce23f0fae9b050bf59f1cdb2
Sara Ghali <45029076+SaraGhali@users.noreply.github.com>
2026-03-30 14:55:15 +0200

    fix products

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Store } from '../../Models/Store';
@@ -7,6 +7,7 @@ import { ICategory } from '../../Models/ICategory';
 import { ProductCard } from '../product-card/product-card';
 import { ProductDetailsComponent } from '../product-details/product-details';
 import { CreditCardFormatPipe } from '../../pipes/credit-card-format-pipe';
+import { ProductsService } from '../../services/productsService';
 
 @Component({
   selector: 'app-products',
@@ -24,17 +25,11 @@ export class Products {
   StoreOwner: string = 'Sara Ghali';
   currentDate: Date = new Date();
   creditCardNumber: string = '1234567890123456';
-  ProductList: IProduct[] = [
-    { ID: 1, Name: 'bookA', Quantity: 10, Price: 1000, Img: './images/OIP.jpg', CategoryID: 1 },
-    { ID: 2, Name: 'bookB', Quantity: 0, Price: 20, Img: './images/OIP.jpg', CategoryID: 1 },
-    { ID: 3, Name: 'bookC', Quantity: 2, Price: 15, Img: './images/OIP.jpg', CategoryID: 2 },
-    { ID: 4, Name: 'bookD', Quantity: 5, Price: 500, Img: './images/OIP.jpg', CategoryID: 1 },
-    { ID: 5, Name: 'bookE', Quantity: 0, Price: 20, Img: './images/OIP.jpg', CategoryID: 1 },
-    { ID: 6, Name: 'bookF', Quantity: 2, Price: 15, Img: './images/OIP.jpg', CategoryID: 2 },
-    { ID: 7, Name: 'bookG', Quantity: 5, Price: 500, Img: './images/OIP.jpg', CategoryID: 1 },
-    { ID: 8, Name: 'bookH', Quantity: 10, Price: 1000, Img: './images/OIP.jpg', CategoryID: 1 },
-  ];
 
+
+  private productsService = inject(ProductsService);
+  ProductList: IProduct[] = [];
+  
   categories: ICategory[] = [
     { ID: 1, Name: 'Old Books' },
     { ID: 2, Name: 'New Books' },
@@ -43,18 +38,16 @@ export class Products {
   searchTerm: string = '';
   selectedProduct: IProduct | null = null;
 
+  constructor() {
+    this.ProductList = this.productsService.getProducts();
+  }
+
   get filteredProducts(): IProduct[] {
-    let prods = this.ProductList;
-    if (this.searchTerm) {
-      prods = prods.filter(p => p.Name.toLowerCase().includes(this.searchTerm.toLowerCase()));
-    }
-    return prods;
+    return this.productsService.searchProducts(this.searchTerm);
   }
 
   buy(product: IProduct) {
-    if (product.Quantity > 0) {
-      product.Quantity--;
-    }
+    this.productsService.buy(product);
   }
 
   showDetails(product: IProduct) {
